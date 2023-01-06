5d74a86fff5d3c1f26c131e509e14bf1d2d75493
MathurJi <anileshjjssacademy84@gmail.com>
2023-01-06 19:55:06 +0530

    final project on the tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
+
 import Square from './Square';
 import './App.css';
 
 function App() {
   return (
-    <>
-    <div className="App">
-      <Square/>
+      <div className="App">
+       <Square/>
       
     </div>
-    <div className='game'>
-      <button>4*4 Game</button>
-      <button>5*5 Game</button>
-      </div>
-    </>
   );
 }
 
--- src/SmallSquare.js
+++ src/SmallSquare.js
@@ -2,7 +2,7 @@
 const SmallSquare =(props)=>{
     return (
         <div onClick={props.onClick} className="SmallSqrDiv">
-            <h2>{props.value}</h2>
+            <h2 className="value">{props.value}</h2>
         </div>
     )
 }
